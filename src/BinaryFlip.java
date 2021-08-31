e412a073c494947f708ff21861ce651996fabeab
LijoFernando <rotricklijo@gmail.ocm>
2021-08-31 19:19:22 +0530

    l2 prac

--- src/BinaryFlip.java
+++ src/BinaryFlip.java
@@ -3,16 +3,14 @@ public class BinaryFlip {
         String binaryVal ="010111";
         int size = binaryVal.length()-1;
         Boolean isAlternative = true;
-        for (int i = 0; i <size; i++) {
-            int j=i+1;
-            char firstchar = binaryVal.charAt(i);//  System.out.println(firstchar);
-            char nxtchar = binaryVal.charAt(j);//  System.out.println(nxtchar);
-            if((firstchar == '0' && nxtchar == '0') || (firstchar == '1' && nxtchar =='1') ){
-                isAlternative =false;
-                System.out.println("not alternative");
-                break;
-            }
-            i=j+1;
+        BinaryFlip bflip = new BinaryFlip();
+        if (!binaryVal.contains("11") || !binaryVal.contains("00")) {
+            bflip.minFlip(binaryVal);
         }
+
     }
+    public void minFlip(String s) {
+
+    }
+
 }
