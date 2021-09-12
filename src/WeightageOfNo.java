7cfc35840795a8e3f9b16ee233d6fb3465150a88
LijoFernando <rotricklijo@gmail.com>
2021-09-12 20:17:11 +0530

    L3 practise

--- src/WeightageOfNo.java
+++ src/WeightageOfNo.java
@@ -1,15 +1,15 @@
-import java.util.*;
-
-public class WeightageOfNo {
-    static int[] setOfNumber = {10,36,54,89,12};
-    static List weightList = new ArrayList();
-    static int[] weightage= new int[setOfNumber.length] ;
-    static int score = 0;
-    public int calFact(int no){
-
-    }
-    public static void main(String[] args) {
-
-
-    }
-}
+//import java.util.*;
+//
+//public class WeightageOfNo {
+//    static int[] setOfNumber = {10,36,54,89,12};
+//    static List weightList = new ArrayList();
+//    static int[] weightage= new int[setOfNumber.length] ;
+//    static int score = 0;
+//    public int calFact(int no){
+//
+//    }
+//    public static void main(String[] args) {
+//
+//
+//    }
+//}
